48969f013c16a895f34125fad120cb9276d4fe77
seansta18 <sdmcmillan@wpi.edu>
2022-11-03 13:34:25 -0400

    removed css file and started js for the page

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    const batteryCharge = 0;
+    const pack1Charge = 0; 
+    const pack2Charge = 0; 
+    const pack3Charge = 0; 
+    const pack4Charge = 0; 
+
+    const batteryTemp = 0;
+    const motorTemp = 0;
+    const externalTemp = 0;
+}
+
+const remoteStart = function() {
+    alert("This will either warm or cool the batteries based on temperature");
+}
